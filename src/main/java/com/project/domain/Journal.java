0f8f6ec32e1f4f9db3ecaa54906af2e9ec2b052f
ffekirnew <ffekirnew0808@gmail.com>
2024-01-25 11:20:33 +0300

    nit: Run formatter

--- src/main/java/com/project/domain/Journal.java
+++ src/main/java/com/project/domain/Journal.java
@@ -1,22 +1,20 @@
 package com.project.domain;
 
 public class Journal {
-	public int id;
-	public String title;
-	public String content;
-	public int userId;
-	
-	public Journal(String title, String content) {
-		this.title = title;
-		this.content = content;
-	}
-	
-	public Journal(int id, String title, String content, int userId) {
-		this.id = id;
-		this.title = title;
-		this.content = content;
-		this.userId = userId;
-	}
-	
+  public int id;
+  public String title;
+  public String content;
+  public int userId;
 
+  public Journal(String title, String content) {
+    this.title = title;
+    this.content = content;
+  }
+
+  public Journal(int id, String title, String content, int userId) {
+    this.id = id;
+    this.title = title;
+    this.content = content;
+    this.userId = userId;
+  }
 }
