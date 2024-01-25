5d2455232cb289772f13065606c4db0cf3eb2ce8
ffekirnew <ffekirnew0808@gmail.com>
2024-01-25 11:21:05 +0300

    nit: Run formatter

--- src/main/java/com/project/domain/User.java
+++ src/main/java/com/project/domain/User.java
@@ -1,32 +1,33 @@
 package com.project.domain;
 
 public class User {
-	public int id;
-	public String firstName;
-	public String lastName;
-	public String email;
-	public String password;
-	public String role;
+  public int id;
+  public String firstName;
+  public String lastName;
+  public String email;
+  public String password;
+  public String role;
 
-	public User() {
-	}
+  public User() {
+  }
 
-	public User(String first_name, String last_name, String email, String password, String role) {
-		this.firstName = first_name;
-		this.lastName = last_name;
-		this.email = email;
-		this.password = password;
-		this.role = role;
-	}
+  public User(String firstName, String lastName, String email, String password, String role) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
+    this.role = role;
+  }
 
-	public User(int id, String first_name, String last_name, String email, String password, String role) {
-		this.id = id;
-		this.firstName = first_name;
-		this.lastName = last_name;
-		this.email = email;
-		this.password = password;
-		this.role = role;
-	}
+  public User(int id, String firstName, String lastName, String email, String password, String role) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
+    this.role = role;
+  }
+
   public User(String email, String password) {
     this.email = email;
     this.password = password;
