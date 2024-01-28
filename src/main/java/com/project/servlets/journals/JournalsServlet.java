88d307531ebec5dd5a125ed872131333d6bbfa81
ffekirnew <ffekirnew0808@gmail.com>
2024-01-28 18:02:10 +0300

    feat: Minor changes

--- src/main/java/com/project/servlets/journals/JournalsServlet.java
+++ src/main/java/com/project/servlets/journals/JournalsServlet.java
@@ -1,6 +1,7 @@
 package com.project.servlets.journals;
 
 import com.project.domain.Journal;
+import com.project.domain.User;
 import com.project.services.JournalService;
 
 import java.io.IOException;
@@ -12,28 +13,32 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/journals")
 public class JournalsServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        // Retrieve data from the service
-        List<Journal> journals = null;
-        int userId = Integer.parseInt(request.getParameter("userId"));
-        try {
-            journals = JournalService.getAllJournalEntries(userId);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    // Retrieve data from the service
+    HttpSession session = request.getSession();
+    User user = (User) session.getAttribute("user");
+    List<Journal> journals = null;
+    int userId = user.id;
 
-        // Set the data as an attribute in the request
-        request.setAttribute("journals", journals);
-
-        // Forward the request to the JSP page
-        request.getRequestDispatcher("/user/journals.jsp").forward(request, response);
+    try {
+      journals = JournalService.getAllJournalEntries(userId);
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException(e);
     }
+
+    // Set the data as an attribute in the request
+    request.setAttribute("journals", journals);
+
+    // Forward the request to the JSP page
+    request.getRequestDispatcher("/user/journals.jsp").forward(request, response);
+  }
 }
