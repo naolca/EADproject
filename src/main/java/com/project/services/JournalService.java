9c4fecb8eaa89e85c3726389b6488610d064276b
ffekirnew <ffekirnew0808@gmail.com>
2024-01-28 23:34:23 +0300

    nit: Run formatter

--- src/main/java/com/project/services/JournalService.java
+++ src/main/java/com/project/services/JournalService.java
@@ -1,4 +1,5 @@
 package com.project.services;// JournalService.java
+
 import com.project.domain.Journal;
 import com.project.persistence.repositories.JournalRepository;
 import com.project.persistence.repositories.UnitOfWork;
@@ -7,11 +8,11 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class JournalService {
-    public static List<Journal> getAllJournalEntries(int userId) throws SQLException, ClassNotFoundException {
-        // Replace this with your actual data retrieval logic from the database
-        // For simplicity, let's assume you have a static list
-        UnitOfWork uof = UnitOfWork.getInstance();
-        JournalRepository journalRepository = uof.getJournalRepository();
-        return journalRepository.getJournalsByUserId(userId);
-    }
+  public static List<Journal> getAllJournalEntries(int userId) throws SQLException, ClassNotFoundException {
+    // Replace this with your actual data retrieval logic from the database
+    // For simplicity, let's assume you have a static list
+    UnitOfWork uof = UnitOfWork.getInstance();
+    JournalRepository journalRepository = uof.getJournalRepository();
+    return journalRepository.getJournalsByUserId(userId);
+  }
 }
